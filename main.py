9c24edd3c8b031f581c095a5be585f5687e91de9
SimonAmar26 <150928602+SimonAmar26@users.noreply.github.com>
2023-11-27 18:50:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from turtle import *
 
-AIDAMS = Turtle()
+T = Turtle()
+T.screen.title("Whack a Mole")
+T.screen.bgpic("/Users/simonamar-roisenberg/Desktop/Computional Thinking/BG.gif")
+T.screensize(canvwidth=500, canvheight=300,
+                  bg="black")
+T.color('red')
+T.fillcolor('yellow')
+while True:
+    T.begin_fill()
+    T.forward(200)
+    T.left(170)
+    if abs(T.pos()) < 1:
+        break
+
+T.screen.mainloop()
